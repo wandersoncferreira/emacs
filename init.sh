b0d88e5ab01068e4d15a36652b5e746c28d9eee5
Wanderson Ferreira <iagwanderson@gmail.com>
2020-12-06 00:28:36 -0300

    adjusting init script

--- init.sh
+++ init.sh
@@ -3,10 +3,16 @@ mv $PWD/emacs-profiles.el ~/.emacs-profiles.el
 emacs --with-profile default --daemon=default;
 emacs --with-profile clojure --daemon=clojure;
 
-alias eclj="emacsclient -c -s clojure &"
-alias etclj="emacsclient -t -s clojure"
-
+# GUI instances
 alias emacs="emacsclient -c -s default"
+alias "emacs-default"="emacsclient -c -s default &"
+alias "emacs-clojure"="emacsclient -c -s clojure &"
+
+# terminal instances
 alias et="emacsclient -t -s default"
+alias "et-clojure"="emacsclient -t -s clojure"
+alias "et-default"="emacsclient -t -s default"
+
+
 
 
